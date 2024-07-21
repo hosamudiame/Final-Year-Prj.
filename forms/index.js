005737c48af8dc52c02bcbf6ecfb5b687b3c0b73
hosamudiame <61981229+hosamudiame@users.noreply.github.com>
2024-07-21 17:29:13 +0100

    Add files via upload

---
+++ forms/index.js
@@ -0,0 +1,8 @@
+
+const create_form = document.querySelector('.create_form');
+
+const forms = (e) => {
+    e.preventDefault();
+}
+
+create_form.addEventListener('submit',forms);
---
+++ storeData.js
@@ -0,0 +1,326 @@
+const storeData = [
+  {
+    name: 'GPS 01',
+    image: 'store-a.jpg',
+    coordinates: {
+      lng: 6.45401647,
+      lat: 9.53656125
+    },
+    status: 'active'
+  },
+  {
+    name: 'GPS 02',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.45257950,
+      lat: 9.53693646
+    },
+    status: 'active'
+  },
+  {
+    name: 'GPS 06',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.44941929,
+      lat: 9.53529029
+    },
+    status: 'active'
+  },
+  {
+    name: 'GPS 08',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.45013421,
+      lat: 9.53732587
+    },
+    status: 'active'
+  },
+  {
+    name: 'GPS 10',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.45071812,
+      lat: 9.53596408
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN247S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.545581,
+      lat: 9.599112
+    },
+    status: 'active'
+  },
+  {
+    name: 'N102P',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.558693,
+      lat: 9.639059
+    },
+    status: 'active'
+  },
+  {
+    name: 'FGP (NG14)',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.543351,
+      lat: 9.601568
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN 246S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.542915,
+      lat: 9.602270
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN 245S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.542762,
+      lat: 9.605893
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN 244S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.545428,
+      lat: 9.608796
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN 279S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.564776,
+      lat: 9.615459
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN 281S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.556693,
+      lat: 9.626815
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN 255S',
+    image: '/img/img_CSN 255S.jpg',
+    coordinates: {
+      lng: 6.497911,
+      lat: 9.572894
+    },
+    status: 'dilapidated'
+  },
+  {
+    name: 'CSN 256S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.494049,
+      lat: 9.569072
+    },
+    status: 'destroyed'
+  },
+  {
+    name: 'CSN 128P',
+    image: '/img/img_CSN 128P.jpg',
+    coordinates: {
+      lng: 6.473289,
+      lat: 9.550396
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN 251S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.520470,
+      lat: 9.593977
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN 299S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.510568,
+      lat: 9.628767
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN 300S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.510599,
+      lat: 9.624689
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN 301S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.506260,
+      lat: 9.620338
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN 302S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.501173,
+      lat: 9.622421
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN 303S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.497937,
+      lat: 9.620937
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN 304S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.496093,
+      lat: 9.618950
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN 305S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.493615,
+      lat: 9.617969
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN 306S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.487257,
+      lat: 9.618704
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN 307S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.484890,
+      lat: 9.620404
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN 308S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.482420,
+      lat: 9.622524
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN 309S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.477460,
+      lat: 9.623713
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN174S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.508246,
+      lat: 9.660269
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN261S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.534008,
+      lat: 9.597107
+    },
+    status: 'active'
+  },
+  {
+    name: 'L41',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.506462,
+      lat: 9.585640
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN1735',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.506439,
+      lat: 9.656671
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN271S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.507156,
+      lat: 9.649148
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN167S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.524176,
+      lat: 9.660702
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN168S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.519899,
+      lat: 9.664827
+    },
+    status: 'active'
+  },
+  {
+    name: 'CSN2765S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.512069,
+      lat: 9.666693
+    },
+    status: 'active'
+  }
+];
