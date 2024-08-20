e8f05fbb6d590885b137e7e8da6955c2bd405c37
hosamudiame <61981229+hosamudiame@users.noreply.github.com>
2024-08-20 08:49:03 +0100

    Add files via upload

---
+++ public/img/storeData.js
@@ -0,0 +1,447 @@
+
+const storeData = [
+  {
+    name: 'GPS 01',
+    image: '/img/gps01.jpg',
+    coordinates: {
+      lng: 6.4539189,
+      lat: 9.5366596
+    },
+    N: 1055094.47,
+    E: 220563.7777,
+    Height: 238.178,
+    status: 'active'
+  },
+  {
+    name: 'GPS 02',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.45473,
+      lat: 9.5343662
+    },
+    N: 11054871.04,
+    E: 220520.65,
+    Height: 236.3418,
+    status: 'active'
+  },
+  {
+    name: 'GPS 03',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.4531789,
+      lat: 9.533052
+    },
+    N: 1054695.84,
+    E: 220479.5487,
+    Height: 237.873,
+    status: 'active'
+  },
+  {
+    name: 'GPS 04',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.4516211,
+      lat: 9.5322585
+    },
+    N: 1054606.28,
+    E: 220309.909,
+    Height: 238.3018,
+    status: 'active'
+  },
+  {
+    name: 'GPS 05',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.4498772,
+      lat: 9.5326402
+    },
+    N: 1054652.94,
+    E: 220116.5072,
+    Height: 237.6725,
+    status: 'active'
+  },
+  {
+    name: 'GPS 06',
+    image: '/img/gps06.jpg',
+    coordinates: {
+      lng: 6.4494608,
+      lat: 9.5353113
+    },
+    N: 1054948.89,
+    E: 220072.9568,
+    Height: 235.9954,
+    status: 'active'
+  },
+  {
+    name: 'GPS 07',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.4486191,
+      lat: 9.5365955
+    },
+    N: 1055091.67,
+    E: 219981.5358,
+    Height: 231.9407,
+    status: 'active'
+  },
+  {
+    name: 'GPS 08',
+    image: '/img/gps08.jpg',
+    coordinates: {
+      lng: 6.4501728,
+      lat: 9.5373333
+    },
+    N: 1055172.05,
+    E: 220152.8232,
+    Height: 232.332,
+    status: 'active'
+  },
+  {
+    name: 'GPS 09',
+    image: '/img/gps09.jpg',
+    coordinates: {
+      lng: 6.4526436,
+      lat: 9.5369522
+    },
+    N: 1055127.89,
+    E: 220423.917,
+    Height: 236.533,
+    status: 'active'
+  },
+  {
+    name: 'GPS 10',
+    image: '/img/gps10.jpg',
+    coordinates: {
+      lng: 6.4507214,
+      lat: 9.5359595
+    },
+    N: 1055019.58,
+    E: 220211.9586,
+    Height: 236.2833,
+    status: 'active'
+  },
+  {
+    name: 'GPS 11',
+    image: '/img/gps11.jpg',
+    coordinates: {
+      lng: 6.4519506,
+      lat: 9.5341798
+    },
+    N: 1054820.22,
+    E: 220344.786,
+    Height: 238.3923,
+    status: 'active'
+  },
+  {
+    name: 'GPS 12',
+    image: '/img/gps12.jpg',
+    coordinates: {
+      lng: 6.4512416,
+      lat: 9.5338908
+    },
+    N: 1054790.22,
+    E: 220267.4124,
+    Height: 238.0979,
+    status: 'active'
+  },
+  {
+    name: 'GPS 13',
+    image: '/img/gps13.jpg',
+    coordinates: {
+      lng: 6.4469395,
+      lat: 9.5358133
+    },
+    N: 1055006.47,
+    E: 219796.3889,
+    Height: 228.3051,
+    status: 'active'
+  },
+  {
+    name: 'GPS 14',
+    image: '/img/gps14.jpg',
+    coordinates: {
+      lng: 6.4491373,
+      lat: 9.5322271
+    },
+    N: 1054607.82,
+    E: 220034.8939,
+    Height: 235.909,
+    status: 'active'
+  },
+  {
+    name: 'GPS 15',
+    image: '/img/gps15.jpg',
+    coordinates: {
+      lng: 6.4518552,
+      lat: 9.5295014
+    },
+    N: 1054303.98,
+    E: 220331.2375,
+    Height: 236.8465,
+    status: 'active'
+  },
+  // {
+  //   name: 'GPS 16',
+  //   image: 'store-b.jpg',
+  //   coordinates: {
+  //     lng: 6.4488671,
+  //     lat: 9.5246624
+  //   },
+  //   N: 0,
+  //   E: 0,
+  //   Height: 0,
+  //   status: 'active'
+  // },
+  {
+    name: 'GPS 17',
+    image: '/img/gps17.jpg',
+    coordinates: {
+      lng: 6.4447462,
+      lat: 9.5240577
+    },
+    N: 1054022.88,
+    E: 219257.641,
+    Height: 224.1672,
+    status: 'dilapidated'
+  },
+  {
+    name: 'GPS 18',
+    image: '/img/gps18.jpg',
+    coordinates: {
+      lng: 6.4421017,
+      lat: 9.5268898
+    },
+    N: 1054285.2,
+    E: 218963.219,
+    Height: 214.2603,
+    status: 'destroyed'
+  },
+  {
+    name: 'GPS 19',
+    image: '/img/gps19.jpg',
+    coordinates: {
+      lng: 6.439404,
+      lat: 9.5292404
+    },
+    N: 1054794.96,
+    E: 219001.0997,
+    Height: 216.0039,
+    status: 'active'
+  },
+  {
+    name: 'GPS 20',
+    image: '/img/gps020.jpg',
+    coordinates: {
+      lng: 6.4397145,
+      lat: 9.533849
+    },
+    N: 1054791.87,
+    E: 219485.7142,
+    Height: 216.937,
+    status: 'active'
+  },
+  {
+    name: 'GPS 21',
+    image: '/img/gps21.jpg',
+    coordinates: {
+      lng: 6.4441336,
+      lat: 9.5340523
+    },
+    N: 1054594.17,
+    E: 219725.936,
+    Height: 227.1487,
+    status: 'active'
+  },
+  {
+    name: 'GPS 22',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.4463259,
+      lat: 9.5320832
+    },
+    N: 1054174.53,
+    E: 219921.827,
+    Height: 231.6228,
+    status: 'active'
+  },
+  {
+    name: 'GPS 23',
+    image: '/img/gps23.jpg',
+    coordinates: {
+      lng: 6.4481382,
+      lat: 9.5283045
+    },
+    N: 1054064.78,
+    E: 219646.016,
+    Height: 230.7706,
+    status: 'dilapidated'
+  },
+  {
+    name: 'GPS 24',
+    image: '/img/gps24.jpg',
+    coordinates: {
+      lng: 6.4459988,
+      lat: 9.5272049
+    },
+    N: 1055116.94,
+    E: 222022.8411,
+    Height: 227.6676,
+    status: 'active'
+  },
+  {
+    name: 'GPS 031',
+    image: '/img/gps031.jpg',
+    coordinates: {
+      lng: 6.467199,
+      lat: 9.5369595
+    },
+    N: 1064547.45,
+    E: 225823.3766,
+    Height: 221.8271,
+    status: 'active'
+  },
+  {
+    name: 'CSN 128P',
+    image: '/img/img_CSN 128P.jpg',
+    coordinates: {
+      lng: 6.473289,
+      lat: 9.5504038
+    },
+    N: 1056599.84,       				
+    E: 222702.7821,
+    Height: 229.7032,
+    status: 'active'
+  },
+  {
+    name: 'CSN 256S',
+    image: '/img/CSN256.jpg',
+    coordinates: {
+      lng: 6.4941064,  				
+      lat: 9.5690401
+    },
+    N: 1058649.48,
+    E: 224997.3642,        
+    Height: 227.3991,
+    status: 'active'
+  },
+  {
+    name: 'CSN 255S',
+    image: '/img/img_CSN 255S.jpg',
+    coordinates: {
+      lng: 6.4979185,
+      lat: 9.5728988
+    },
+    N: 1059069.44,
+    E: 225426.1744,
+    Height: 221.5324,
+    status: 'active'
+  },
+  {
+    name: 'L41',
+    image: 'store-b.jpg',     			
+    coordinates: {  
+      lng: 6.5079481,
+      lat: 9.5842869
+    },
+    N: 1060471.97,
+    E: 226374.197,
+    Height: 230.1518,
+    status: 'active'
+  },
+  {
+    name: 'CSN 251S',     				
+    image: '/img/CSN251S.jpg',
+    coordinates: {
+      lng: 6.5201571,
+      lat: 9.5938636
+    },
+    N: 1061382.74,
+    E: 227918.91,
+    Height: 221.1056,
+    status: 'active'
+  },
+  {
+    name: 'CSN261S',        				
+    image: '/img/csn261s.jpg',
+    coordinates: {
+      lng: 6.5338647,
+      lat: 9.5972435
+    },
+    N: 1061734.92,
+    E: 229393.6101,
+    Height: 228.5029,
+    status: 'active'
+  },
+  {
+    name: 'CSN 302S',
+    image: '/img/csn302s.jpg',
+    coordinates: {
+      lng: 6.5011717,
+      lat: 9.6224328    
+    }, 
+    N: 1064547.45,
+    E: 225823.3766,
+    Height: 240.7643,
+    status: 'dilapidated'
+  },
+  {
+    name: 'N102P',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.558693,
+      lat: 9.639059
+    },
+    N: 1066341.61,
+    E: 232153.32,
+    Height: 0,
+    status: 'active'
+  },
+  {
+    name: 'CSN 279S',
+    image: '/img/CSN279S.jpg',
+    coordinates: {
+      lng: 6.564776,
+      lat: 9.615459
+    },
+    N: 1063726.09,
+    E: 232801.991,
+    Height: 280.128,
+    status: 'active'
+  },
+  {
+    name: 'CSN 244S',
+    image: '/img/CSN244S.jpg',
+    coordinates: {
+      lng: 6.545428,
+      lat: 9.608796
+    },
+    N: 1063004.26,
+    E: 230672.974,
+    Height: 243.816,
+    status: 'active'
+  },
+  {
+    name: 'CSN 245S',
+    image: '/img/CSN245S.jpg',
+    coordinates: {
+      lng: 6.542762,
+      lat: 9.605893
+    },
+    N: 1062684.1,
+    E: 230377.593,
+    Height: 242.893,
+    status: 'active'
+  },
+  {
+    name: 'CSN 246S',
+    image: '/img/CSN246.jpg',
+    coordinates: {
+      lng: 6.542915,
+      lat: 9.602270
+    },
+    N: 1062283.000,
+    E: 230391.498,
+    Height: 0,
+    status: 'active'
+  },
+];
