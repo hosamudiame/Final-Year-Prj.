d4de70c2ad7457e0f2d093ff0e3f4b6a86530c05
hosamudiame <61981229+hosamudiame@users.noreply.github.com>
2024-08-20 07:38:31 +0100

    Add files via upload

--- public/js/storeData.js
+++ public/js/storeData.js
@@ -7,7 +7,7 @@ const storeData = [
       lng: 6.4539189,
       lat: 9.5366596
     },
-    N: 1055094.47,
+    N: 1055094.465,
     E: 220563.7777,
     Height: 238.178,
     status: 'active'
@@ -19,8 +19,8 @@ const storeData = [
       lng: 6.45473,
       lat: 9.5343662
     },
-    N: 11054871.04,
-    E: 220520.65,
+    N: 1054840,
+    E: 220651.003,
     Height: 236.3418,
     status: 'active'
   },
@@ -31,7 +31,7 @@ const storeData = [
       lng: 6.4531789,
       lat: 9.533052
     },
-    N: 1054695.84,
+    N: 1054695.839,
     E: 220479.5487,
     Height: 237.873,
     status: 'active'
@@ -43,8 +43,8 @@ const storeData = [
       lng: 6.4516211,
       lat: 9.5322585
     },
-    N: 1054606.28,
-    E: 220309.909,
+    N: 1054609.279,
+    E: 220307.7657,
     Height: 238.3018,
     status: 'active'
   },
@@ -67,7 +67,7 @@ const storeData = [
       lng: 6.4494608,
       lat: 9.5353113
     },
-    N: 1054948.89,
+    N: 1054948.888,
     E: 220072.9568,
     Height: 235.9954,
     status: 'active'
@@ -79,7 +79,7 @@ const storeData = [
       lng: 6.4486191,
       lat: 9.5365955
     },
-    N: 1055091.67,
+    N: 1055091.669,
     E: 219981.5358,
     Height: 231.9407,
     status: 'active'
@@ -91,7 +91,7 @@ const storeData = [
       lng: 6.4501728,
       lat: 9.5373333
     },
-    N: 1055172.05,
+    N: 1055172.047,
     E: 220152.8232,
     Height: 232.332,
     status: 'active'
@@ -103,7 +103,7 @@ const storeData = [
       lng: 6.4526436,
       lat: 9.5369522
     },
-    N: 1055127.89,
+    N: 1055127.885,
     E: 220423.917,
     Height: 236.533,
     status: 'active'
@@ -115,7 +115,7 @@ const storeData = [
       lng: 6.4507214,
       lat: 9.5359595
     },
-    N: 1055019.58,
+    N: 1055019.578,
     E: 220211.9586,
     Height: 236.2833,
     status: 'active'
@@ -127,8 +127,8 @@ const storeData = [
       lng: 6.4519506,
       lat: 9.5341798
     },
-    N: 1054820.22,
-    E: 220344.786,
+    N: 1054821.641,
+    E: 220345.5349,
     Height: 238.3923,
     status: 'active'
   },
@@ -139,7 +139,7 @@ const storeData = [
       lng: 6.4512416,
       lat: 9.5338908
     },
-    N: 1054790.22,
+    N: 1054790.223,
     E: 220267.4124,
     Height: 238.0979,
     status: 'active'
@@ -151,7 +151,7 @@ const storeData = [
       lng: 6.4469395,
       lat: 9.5358133
     },
-    N: 1055006.47,
+    N: 1055006.465,
     E: 219796.3889,
     Height: 228.3051,
     status: 'active'
@@ -163,7 +163,7 @@ const storeData = [
       lng: 6.4491373,
       lat: 9.5322271
     },
-    N: 1054607.82,
+    N: 1054607.823,
     E: 220034.8939,
     Height: 235.909,
     status: 'active'
@@ -175,7 +175,7 @@ const storeData = [
       lng: 6.4518552,
       lat: 9.5295014
     },
-    N: 1054303.98,
+    N: 1054303.978,
     E: 220331.2375,
     Height: 236.8465,
     status: 'active'
@@ -199,8 +199,8 @@ const storeData = [
       lng: 6.4447462,
       lat: 9.5240577
     },
-    N: 1054022.88,
-    E: 219257.641,
+    N: 1053707.313,
+    E: 219545.8412,
     Height: 224.1672,
     status: 'dilapidated'
   },
@@ -211,8 +211,8 @@ const storeData = [
       lng: 6.4421017,
       lat: 9.5268898
     },
-    N: 1054285.2,
-    E: 218963.219,
+    N: 1054022.877,
+    E: 219257.641,
     Height: 214.2603,
     status: 'destroyed'
   },
@@ -223,8 +223,8 @@ const storeData = [
       lng: 6.439404,
       lat: 9.5292404
     },
-    N: 1054794.96,
-    E: 219001.0997,
+    N: 1054285.195,
+    E: 218963.219,
     Height: 216.0039,
     status: 'active'
   },
@@ -235,8 +235,8 @@ const storeData = [
       lng: 6.4397145,
       lat: 9.533849
     },
-    N: 1054791.87,
-    E: 219485.7142,
+    N: 1054794.963,
+    E: 219001.0997,
     Height: 216.937,
     status: 'active'
   },
@@ -247,8 +247,8 @@ const storeData = [
       lng: 6.4441336,
       lat: 9.5340523
     },
-    N: 1054594.17,
-    E: 219725.936,
+    N: 1054813.867,
+    E: 219486.7142,
     Height: 227.1487,
     status: 'active'
   },
@@ -259,8 +259,8 @@ const storeData = [
       lng: 6.4463259,
       lat: 9.5320832
     },
-    N: 1054174.53,
-    E: 219921.827,
+    N: 1054594.17,
+    E: 219725.936,
     Height: 231.6228,
     status: 'active'
   },
@@ -271,8 +271,8 @@ const storeData = [
       lng: 6.4481382,
       lat: 9.5283045
     },
-    N: 1054064.78,
-    E: 219646.016,
+    N: 1054174.534,
+    E: 219921.9315,
     Height: 230.7706,
     status: 'dilapidated'
   },
@@ -283,8 +283,8 @@ const storeData = [
       lng: 6.4459988,
       lat: 9.5272049
     },
-    N: 1055116.94,
-    E: 222022.8411,
+    N: 1054054.578,
+    E: 219686.0216,
     Height: 227.6676,
     status: 'active'
   },
@@ -295,8 +295,8 @@ const storeData = [
       lng: 6.467199,
       lat: 9.5369595
     },
-    N: 1064547.45,
-    E: 225823.3766,
+    N: 1055116.937,
+    E: 222022.8411,
     Height: 221.8271,
     status: 'active'
   },
@@ -307,21 +307,9 @@ const storeData = [
       lng: 6.473289,
       lat: 9.5504038
     },
-    N: 1056599.84,       				
+    N: 1056599.843,       				
     E: 222702.7821,
     Height: 229.7032,
-    status: 'active'
-  },
-  {
-    name: 'CSN 256S',
-    image: '/img/CSN256.jpg',
-    coordinates: {
-      lng: 6.4941064,  				
-      lat: 9.5690401
-    },
-    N: 1058649.48,
-    E: 224997.3642,        
-    Height: 227.3991,
     status: 'active'
   },
   {
@@ -331,9 +319,21 @@ const storeData = [
       lng: 6.4979185,
       lat: 9.5728988
     },
-    N: 1059069.44,
+    N: 1059069.443,
     E: 225426.1744,
     Height: 221.5324,
+    status: 'active'
+  },
+  {
+    name: 'CSN 256S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.4941064,  				
+      lat: 9.5690401
+    },
+    N: 1058645.478,
+    E: 225004.3642,        
+    Height: 227.3991,
     status: 'active'
   },
   {
@@ -343,8 +343,8 @@ const storeData = [
       lng: 6.5079481,
       lat: 9.5842869
     },
-    N: 1060471.97,
-    E: 226374.197,
+    N: 1060321.668,
+    E: 226536.897,
     Height: 230.1518,
     status: 'active'
   },
@@ -355,8 +355,8 @@ const storeData = [
       lng: 6.5201571,
       lat: 9.5938636
     },
-    N: 1061382.74,
-    E: 227918.91,
+    N: 1061371.735,
+    E: 227885.4656,
     Height: 221.1056,
     status: 'active'
   },
@@ -367,7 +367,7 @@ const storeData = [
       lng: 6.5338647,
       lat: 9.5972435
     },
-    N: 1061734.92,
+    N: 1061734.919,
     E: 229393.6101,
     Height: 228.5029,
     status: 'active'
@@ -379,10 +379,22 @@ const storeData = [
       lng: 6.5011717,
       lat: 9.6224328    
     }, 
-    N: 1064547.45,
+    N: 1064548.35,
     E: 225823.3766,
     Height: 240.7643,
     status: 'dilapidated'
+  },
+  {
+    name: 'CSN247S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.545581,
+      lat: 9.599112
+    },
+    N: 0,
+    E: 0,
+    Height: 0,
+    status: 'active'
   },
   {
     name: 'N102P',
@@ -391,57 +403,165 @@ const storeData = [
       lng: 6.558693,
       lat: 9.639059
     },
-    N: 1066341.61,
-    E: 232153.32,
+    N: 0,
+    E: 0,
+    Height: 0,
+    status: 'active'
+  },
+  {
+    name: 'FGP (NG14)',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.543351,
+      lat: 9.601568
+    },
+    N: 0,
+    E: 0,
+    Height: 0,
+    status: 'active'
+  },
+  {
+    name: 'CSN 246S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.542915,
+      lat: 9.602270
+    },
+    N: 0,
+    E: 0,
+    Height: 0,
+    status: 'active'
+  },
+  {
+    name: 'CSN 245S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.542762,
+      lat: 9.605893
+    },
+    N: 0,
+    E: 0,
+    Height: 0,
+    status: 'active'
+  },
+  {
+    name: 'CSN 244S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.545428,
+      lat: 9.608796
+    },
+    N: 0,
+    E: 0,
     Height: 0,
     status: 'active'
   },
   {
     name: 'CSN 279S',
-    image: '/img/img/CSN279S',
+    image: 'store-b.jpg',
     coordinates: {
       lng: 6.564776,
       lat: 9.615459
     },
-    N: 1063726.09,
-    E: 232801.991,
-    Height: 280.128,
-    status: 'active'
-  },
-  {
-    name: 'CSN 244S',
-    image: '/img/img/CSN244S',
-    coordinates: {
-      lng: 6.545428,
-      lat: 9.608796
-    },
-    N: 1063004.26,
-    E: 230672.974,
-    Height: 243.816,
-    status: 'active'
-  },
-  {
-    name: 'CSN 245S',
-    image: '/img/img/CSN245S',
-    coordinates: {
-      lng: 6.542762,
-      lat: 9.605893
-    },
-    N: 1062684.1,
-    E: 230377.593,
-    Height: 242.893,
-    status: 'active'
-  },
-  {
-    name: 'CSN 246S',
-    image: '/img/img/CSN246S',
-    coordinates: {
-      lng: 6.542915,
-      lat: 9.602270
-    },
-    N: 1062283.000,
-    E: 230391.498,
+    N: 0,
+    E: 0,
     Height: 0,
     status: 'active'
   },
+  {
+    name: 'CSN 281S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.556693,
+      lat: 9.626815
+    },
+    N: 0,
+    E: 0,
+    Height: 0,
+    status: 'active'
+  },
+  {
+    name: 'CSN 302S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.501173,
+      lat: 9.622421
+    },
+    N: 0,
+    E: 0,
+    Height: 0,
+    status: 'active'
+  },
+  {
+    name: 'CSN174S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.508246,
+      lat: 9.660269
+    },
+    N: 0,
+    E: 0,
+    Height: 0,
+    status: 'active'
+  },
+  {
+    name: 'CSN1735',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.506439,
+      lat: 9.656671
+    },
+    N: 0,
+    E: 0,
+    Height: 0,
+    status: 'active'
+  },
+  {
+    name: 'CSN271S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.507156,
+      lat: 9.649148
+    },
+    N: 0,
+    E: 0,
+    Height: 0,
+    status: 'active'
+  },
+  {
+    name: 'CSN167S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.524176,
+      lat: 9.660702
+    },
+    N: 0,
+    E: 0,
+    Height: 0,
+    status: 'active'
+  },
+  {
+    name: 'CSN168S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.519899,
+      lat: 9.664827
+    },
+    N: 0,
+    E: 0,
+    Height: 0,
+    status: 'active'
+  },
+  {
+    name: 'CSN2765S',
+    image: 'store-b.jpg',
+    coordinates: {
+      lng: 6.512069,
+      lat: 9.666693
+    },
+    N: 0,
+    E: 0,
+    Height: 0,
+    status: 'active'
+  }
 ];
